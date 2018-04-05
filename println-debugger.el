080ea856ac2f107d2ec8c4d7fb42b766388bacf2
Josef Vlach <vlach.josef@gmail.com>
2018-04-05 13:13:21 +0100

    Added support for elisp and javascript

--- println-debugger.el
+++ println-debugger.el
@@ -1,6 +1,6 @@
 ;;; println-debugger.el --- Convenient function for generating println statements
 
-;; Copyright (c) 2017 Josef Vlach
+;; Copyright (c) 2018 Josef Vlach
 
 ;; Homepage: https://github.com/VlachJosef/println-debugger
 ;; Package-Version:  0.1
@@ -16,33 +16,41 @@
 (require 'subr-x)
 (require 'ensime)
 
+(defvar println-scala "println(\"%s: \" + (%s))")
+(defvar println-emacs-lisp "(message \"%s: %%s\" %s)")
+(defvar println-javascript "console.log('%s', %s);")
+
+(defun print-ln-print (prompt)
+  (let ((num (prefix-numeric-value arg))
+        (on-empty-line (string-blank-p (thing-at-point 'line t))))
+    (when (> num 0)
+      (when (equal num 1)
+        (unless on-empty-line
+          (beginning-of-line)
+          (ensime-company-complete-or-indent)
+          (kill-line)))
+
+      (when (and (> num 1) (not on-empty-line))
+        (move-beginning-of-line nil)
+        (insert "\n")
+        (previous-line))
+
+      (dotimes (number num)
+        (let* ((index (- num number 1))
+               (latest-kill (current-kill index t))
+               (latest-kill-unquoted (s-replace "\"" " " latest-kill)))
+          (ensime-company-complete-or-indent)
+          (insert (format prompt latest-kill-unquoted latest-kill))
+          (move-end-of-line nil)
+          (unless (equal index 0)
+            (insert "\n")))))))
+
 (defun print-ln (&optional arg)
   (interactive "P")
   (pcase major-mode
-    ('scala-mode
-     (let ((num (prefix-numeric-value arg))
-           (on-empty-line (string-blank-p (thing-at-point 'line t))))
-       (when (> num 0)
-         (when (equal num 1)
-           (unless on-empty-line
-             (beginning-of-line)
-             (ensime-company-complete-or-indent)
-             (kill-line)))
-
-         (when (and (> num 1) (not on-empty-line))
-           (move-beginning-of-line nil)
-           (insert "\n")
-           (previous-line))
-
-         (dotimes (number num)
-           (let* ((index (- num number 1))
-                  (latest-kill (current-kill index t))
-                  (latest-kill-unquoted (s-replace "\"" " " latest-kill)))
-             (ensime-company-complete-or-indent)
-             (insert (format "println(\"%s: \" + (%s))" latest-kill-unquoted latest-kill))
-             (move-end-of-line nil)
-             (unless (equal index 0)
-               (insert "\n")))))))))
+    (`emacs-lisp-mode (print-ln-print println-emacs-lisp))
+    (`js-mode (print-ln-print println-javascript))
+    (`scala-mode (print-ln-print println-scala))))
 
 (provide 'println-debugger)
 ;;; println-debugger.el ends here
--- test/println-debugger-test.el
+++ test/println-debugger-test.el
@@ -1,3 +1,8 @@
+;; Needs emacs 25
+;;
+;; Quick way how to run with desired version:
+;; EMACS=/Applications/Emacs.app/Contents/MacOS/Emacs cask exec ert-runner
+
 (ert-deftest pd:debugger-1 ()
   (should
    (equal
@@ -93,4 +98,25 @@ object Hello {
   println(\"hello2: \" + (hello2))
   world
 }
+")))
+
+(ert-deftest pd:debugger-5 ()
+  (should
+   (equal
+    (with-current-buffer (find-file-noselect "test-elisp-1.el")
+       (progn
+         (insert "(defun println-elisp ()
+  (beginning-of-line)
+|
+  (kill-line))
+")
+         (re-search-backward "|")
+         (delete-char 1)
+         (kill-new "hello1")
+         (funcall-interactively 'print-ln 1)
+         (buffer-string)))
+    "(defun println-elisp ()
+  (beginning-of-line)
+  (message \"hello1: %s\" hello1)
+  (kill-line))
 ")))

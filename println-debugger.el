e669af7f30aabd0d58f3f4a5c6c2ad8f9a6b5f02
Josef Vlach <vlach.josef@gmail.com>
2021-02-13 11:25:53 +0000

    Fix issue when package is loaded before println-debuggger

--- println-debugger.el
+++ println-debugger.el
@@ -13,9 +13,24 @@
 
 (require 'println-debugger-common)
 
-(with-eval-after-load 'elisp-mode (require 'println-debugger-emacs-lisp))
-(with-eval-after-load 'scala-mode (require 'println-debugger-scala))
-(with-eval-after-load 'js-mode (require 'println-debugger-javascript))
-(with-eval-after-load 'gdscript-mode (require 'println-debugger-gdscript))
+(if (featurep 'elisp-mode)
+    (require 'println-debugger-emacs-lisp)
+  (with-eval-after-load 'elisp-mode
+    (require 'println-debugger-emacs-lisp)))
+
+(if (featurep 'scala-mode)
+    (require 'println-debugger-scala)
+  (with-eval-after-load 'scala-mode
+    (require 'println-debugger-scala)))
+
+(if (featurep 'js)
+    (require 'println-debugger-javascript)
+  (with-eval-after-load 'js
+    (require 'println-debugger-javascript)))
+
+(if (featurep 'gdscript-mode)
+    (require 'println-debugger-gdscript)
+  (with-eval-after-load 'gdscript-mode
+    (require 'println-debugger-gdscript)))
 
 (provide 'println-debugger)
